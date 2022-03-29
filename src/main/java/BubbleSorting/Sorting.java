a31c753e71bf26dace8171462fd2a744ce62f9d5
Lev <noviykomp13@mail.ru>
2022-03-29 21:29:00 +0300

    initial commit

---
+++ src/main/java/BubbleSorting/Sorting.java
@@ -0,0 +1,14 @@
+package BubbleSorting;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Sorting {
+
+    public static void main(String[] args) {
+
+        List<Integer> list_to_sort = new LinkedList<>();
+
+
+    }
+}
